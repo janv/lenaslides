1f5a18727fd5e879879593cd083816ae7aea6b8b
Jan Varwig <jan@varwig.org>
2015-08-17 10:31:21 +0200

    Don’t preload all videos and move to next when video ends

--- src/slideshow/lp_slide-directive.js
+++ src/slideshow/lp_slide-directive.js
@@ -1,10 +1,25 @@
 'use strict';
 
-module.exports = function(){
+module.exports = [ '$location', function($location){
   return {
     scope: true,
     controller: require('./lp_slide-controller.js'),
-    controllerAs: 'slide'
+    controllerAs: 'slide',
+    link: function(scope, elem){
+      elem.find('video').on('ended', function(e){
+        var id = scope.slideshow.getNextId();
+        $location.path(id);
+      });
+
+      scope.$watch('slide.isVisible()', function(visible, old){
+        var video = angular.element(elem).find('video')[0];
+        if (visible) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      });
+    }
   };
-};
+}];
 

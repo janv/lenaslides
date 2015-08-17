7d974a58ccdf2fb7cb70054d02c4d4b0caf9b8f5
Jan Varwig <jan@varwig.org>
2015-08-17 10:29:36 +0200

    Improve webpack/dev setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,13 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './src/index.js'
+  ],
   output: {
+    publicPath: '//localhost:8080/',
     path: path.resolve('./public'),
     filename: 'application.js'
   },
@@ -13,11 +18,19 @@ module.exports = {
     loaders: [
       {test: /\.html$/, loader: 'html'},
       {test: /\.sass$/, loader: 'style!css!sass?indentedSyntax'},
-      {test: /\.png$/,  loader: 'url-loader?limit=10000' },
+      {test: /\.png$/,  loader: 'url-loader?limit=10000' }
     ]
   },
-  //plugins: [
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './public',
+    publicPath: '/',
+    colors: true,
+    hot: true
+  },
+  plugins: [
     //new webpack.optimize.UglifyJsPlugin({minimize: true})
-  //]
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
 

44c1d94d9ccf30de0527b569b8336b127a3775cc
Jan Varwig <jan@varwig.org>
2015-08-17 10:50:32 +0200

    Use ng-repeat for slide generation

--- src/application-controller.js
+++ src/application-controller.js
@@ -2,6 +2,16 @@
 
 function ApplicationController($scope, ngAudio, $location, $route){
   this.slideshow = null;
+  var slideNumbers = [ 1,  2,  3,  4,  5,      7,  8,  9, 10,
+                      11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                      21, 22, 23, 24, 25 ];
+  this.slideDatas = slideNumbers.map(function(no){
+    return {
+      id: '' + no,
+      video: 'video/' + no + '.mp4',
+      poster: 'video/' + no + '.png'
+    };
+  });
 
   $scope.$watch('slideshow.currentSlide', slideChanged);
   $scope.$on('$routeChangeSuccess', routeChanged);
